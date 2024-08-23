706299dfc515c3d50225d7ce0542d08f7ca24fde
Aravind <aravind@zaigoinfotech.com>
2024-08-23 18:16:08 +0530

    Changes

--- config/config.go
+++ config/config.go
@@ -1,40 +1,35 @@
 package config
 
-import "os"
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 type EnvModel struct {
 	Port       string `mapstructure:"PORT"`
-	DbName     string `mapstructure:"DB_NAME"`
-	DbUser     string `mapstructure:"DB_USER"`
+	DbName     string `mapstructure:"POSTGRES_DATABASE"`
+	DbUser     string `mapstructure:"POSTGRES_USER"`
 	DbPassword string `mapstructure:"DB_PASSWORD"`
 	DbHost     string `mapstructure:"DB_HOST"`
 	DbPort     string `mapstructure:"DB_PORT"`
 	JWTSecret  string `mapstructure:"JWT_SECRET"`
+	DBUrl      string `mapstructure:"POSTGRES_URL"`
 }
 
 func InitConfig() (configs *EnvModel) {
 
-	// viper.AddConfigPath("./")
-	// viper.SetConfigFile(".env")
-	// viper.SetConfigType("env")
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
 
-	// if err := viper.ReadInConfig(); err != nil {
-	// 	log.Fatal("Error loading env env variables", err)
-	// }
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Error loading env env variables", err)
+	}
 
-	// if err := viper.Unmarshal(&configs); err != nil {
-	// 	log.Fatal("Error while unmarshalling loaded variables into struct")
-	// }
+	if err := viper.Unmarshal(&configs); err != nil {
+		log.Fatal("Error while unmarshalling loaded variables into struct")
+	}
 
-	configs = &EnvModel{}
-
-	configs.Port = os.Getenv("PORT")
-	configs.DbName = os.Getenv("POSTGRES_DATABASE")
-	configs.DbUser = os.Getenv("POSTGRES_USER")
-	configs.DbPassword = os.Getenv("POSTGRES_PASSWORD")
-	configs.DbHost = os.Getenv("POSTGRES_HOST")
-	configs.DbPort = os.Getenv("DB_PORT")
-	configs.JWTSecret = os.Getenv("JWT_SECRET")
-
-	return
+	return configs
 }
--- db/database.go
+++ db/database.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"os"
-
 	"github.com/aravind-m-s/anakallumkal-backend/config"
 	"github.com/aravind-m-s/anakallumkal-backend/domain"
 	"gorm.io/driver/postgres"
@@ -10,8 +8,15 @@ import (
 )
 
 func InitDatabase(cnf *config.EnvModel) (*gorm.DB, error) {
-	psqlInfo := os.Getenv("POSTGRES_URL")
+	// psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cnf.DbHost, cnf.DbUser, cnf.DbName, cnf.DbPort, cnf.DbPassword)
+	psqlInfo := cnf.DBUrl
 	db, err := gorm.Open(postgres.Open(psqlInfo))
+
+	if err != nil {
+		return db, err
+	}
+
+	db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`)
 
 	db.AutoMigrate(&domain.Brand{})
 	db.AutoMigrate(&domain.Furniture{})

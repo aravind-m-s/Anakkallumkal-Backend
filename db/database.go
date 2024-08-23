12a2fe558cbd370763500d70c1dbe26c840bd124
Aravind <aravind@zaigoinfotech.com>
2024-08-23 18:30:16 +0530

    gorm changes

--- db/database.go
+++ db/database.go
@@ -5,12 +5,19 @@ import (
 	"github.com/aravind-m-s/anakallumkal-backend/domain"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 func InitDatabase(cnf *config.EnvModel) (*gorm.DB, error) {
 	// psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cnf.DbHost, cnf.DbUser, cnf.DbName, cnf.DbPort, cnf.DbPassword)
 	psqlInfo := cnf.DBUrl
-	db, err := gorm.Open(postgres.Open(psqlInfo))
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent), // Disable or change logging level
+		NamingStrategy: schema.NamingStrategy{
+			NoLowerCase:   true, // Disable automatic schema name case conversion
+			SingularTable: true,
+		}})
 
 	if err != nil {
 		return db, err
